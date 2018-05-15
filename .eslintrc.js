62c96f54e65c7c17db37cd7955c202c6bf29fdeb
JosefJezek <josef.jezek@inetdata.cz>
2018-05-15 15:25:51 +0200

    Add lint:es npm script

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,8 +34,6 @@ module.exports = {
       },
     ],
 
-    // "import/first": 0,
-
     // Recommend not to leave any console.log in your code.
     // Use console.error, console.warn and console.info instead.
     // https://eslint.org/docs/rules/no-console
